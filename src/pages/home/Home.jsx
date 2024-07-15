d2b20aa4df610be9f290744c77033c5256ef3e8d
dreammehedi <dreammehedihassan@gmail.com>
2024-07-15 08:02:04 +0600

    Deals Of The Day Product Added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import { Helmet } from "react-helmet-async";
 import Banner from "../../components/home/Banner";
 import NextTopBannerProdduct from "../../components/home/NextTopBannerProdduct";
 import DailyBestSales from "./daily_best_sales/DailyBestSales";
+import DealsOfTheDay from "./deals_of_the_day/DealsOfTheDay";
 import PopularProducts from "./popular_products/PopularProducts";
 
 function Home() {
@@ -14,6 +15,7 @@ function Home() {
       <NextTopBannerProdduct></NextTopBannerProdduct>
       <PopularProducts></PopularProducts>
       <DailyBestSales></DailyBestSales>
+      <DealsOfTheDay></DealsOfTheDay>
     </>
   );
 }
