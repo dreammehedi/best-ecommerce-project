c8af3929f5811d6a33d29cac742072057b4dfd2c
dreammehedi <dreammehedihassan@gmail.com>
2024-08-05 16:45:52 +0600

    User Token Get

--- src/hooks/user/useUserData.jsx
+++ src/hooks/user/useUserData.jsx
@@ -1,25 +1,14 @@
 import { useEffect, useState } from "react";
-import useAxiosSecure from "../axios/useAxiosSecure";
 
 function useUserData() {
   const [user, setUser] = useState(null);
-  const [userLoader, setUserLoader] = useState(true);
-  const axiosSecure = useAxiosSecure();
-
+  const [userLoader, setUserLoader] = useState(false);
   useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const response = await axiosSecure.get("/user");
-        const resData = await response.data;
-        console.log("resdata", resData);
-        setUser(resData);
-        setUserLoader(false);
-      } catch (error) {
-        setUserLoader(false);
-      }
-    };
-    getUserData();
-  }, [axiosSecure]);
+    setUserLoader(true);
+    const getToken = JSON.parse(localStorage.getItem("token")) || null;
+    setUser(getToken);
+    setUserLoader(false);
+  }, []);
 
   return { user, userLoader };
 }
