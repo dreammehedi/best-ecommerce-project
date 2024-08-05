3573d30e88ae6986c76e21919bfe93b0687d1c7d
dreammehedi <dreammehedihassan@gmail.com>
2024-08-05 16:53:09 +0600

    Token Get and Decode with jwt and user data get

--- src/hooks/user/useUserData.jsx
+++ src/hooks/user/useUserData.jsx
@@ -1,13 +1,30 @@
+import { jwtDecode } from "jwt-decode";
 import { useEffect, useState } from "react";
 
 function useUserData() {
   const [user, setUser] = useState(null);
   const [userLoader, setUserLoader] = useState(false);
+
   useEffect(() => {
-    setUserLoader(true);
-    const getToken = JSON.parse(localStorage.getItem("token")) || null;
-    setUser(getToken);
-    setUserLoader(false);
+    const userDataFindInToken = async () => {
+      setUserLoader(true);
+      try {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+          const decodedToken = jwtDecode(token);
+          console.log(decodedToken);
+          setUser(decodedToken);
+        } else {
+          console.error("No token found");
+        }
+      } catch (error) {
+        console.error("Invalid token", error);
+      } finally {
+        setUserLoader(false);
+      }
+    };
+    userDataFindInToken();
   }, []);
 
   return { user, userLoader };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,16 +3,12 @@ import Banner from "../../components/home/Banner";
 import GreatOpotunityHome from "../../components/home/GreatOpotunityHome";
 import NewsletterHome from "../../components/home/NewsletterHome";
 import NextTopBannerProdduct from "../../components/home/NextTopBannerProdduct";
-import useUserData from "../../hooks/user/useUserData";
 import CategoryBasedProducts from "./category_bases_products/CategoryBasedProducts";
 import DailyBestSales from "./daily_best_sales/DailyBestSales";
 import DealsOfTheDay from "./deals_of_the_day/DealsOfTheDay";
 import PopularProducts from "./popular_products/PopularProducts";
 
 function Home() {
-  const { user, userLoader } = useUserData();
-  console.log(user, userLoader);
-
   return (
     <>
       <Helmet>
